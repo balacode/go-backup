c8cd531d41433c2482540775260c51556f96f84d
Ali Bala <balarabe@protonmail.com>
2022-05-07 15:43:01 +0100

    feat: create main.go, go.mod, go.sum

---
+++ main.go
@@ -0,0 +1,18 @@
+// -----------------------------------------------------------------------------
+// go-backup/main.go
+
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	runCommand(os.Args)
+}
+
+// runCommand _ _
+func runCommand(osArgs []string) {
+}
+
+// end
