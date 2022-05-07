dd6a89e1fd09e7903fd1c7c04b5ab4f278908a9a
Ali Bala <balarabe@protonmail.com>
2022-05-07 15:56:28 +0100

    feat: create Version and ArchiveExt constants

---
+++ cons/constants.go
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------------------
+// go-backup/cons/constants.go
+
+package cons
+
+// Version is the current version of this utility.
+const Version = "0.0.1"
+
+// ArchiveExt specifies the file extension of archives.
+const ArchiveExt = ".e.gob"
+
+// end
