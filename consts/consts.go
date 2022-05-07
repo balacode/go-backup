45a744263786f5a245c1b30e0b84816b6aa2248c
Ali Bala <balarabe@protonmail.com>
2022-05-07 16:28:07 +0100

    feat: create Action type and constants

--- consts/consts.go
+++ consts/consts.go
@@ -9,4 +9,26 @@ const Version = "0.0.1"
 // ArchiveExt specifies the file extension of archives.
 const ArchiveExt = ".e.gob"
 
+// -----------------------------------------------------------------------------
+
+// Action specifies archiving actions, for example:
+// CreateArchive, ExtractArchive, ListArchive, VerifyArchive
+type Action int
+
+// CreateArchive is an action to create a new archive
+// file from the files in the specified source folder.
+const CreateArchive = Action(1)
+
+// ExtractArchive is an action to read an existing archive
+// file and extract its contents into the specified folder.
+const ExtractArchive = Action(2)
+
+// ListArchive is an action to list all the files stored
+// in an archive file, without extracting them.
+const ListArchive = Action(3)
+
+// VerifyArchive is an action to checks the integrity of all
+// files stored in an archive file (by checking file hashes).
+const VerifyArchive = Action(4)
+
 // end
