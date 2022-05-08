e1afba7cf03f25358ac8ea14158847131c55472d
Ali Bala <balarabe@protonmail.com>
2022-05-08 10:06:18 +0100

    refactor: move TimestampFormat to 'consts' pkg.

--- consts/consts.go
+++ consts/consts.go
@@ -9,6 +9,9 @@ const Version = "0.0.1"
 // ArchiveExt specifies the file extension of archives.
 const ArchiveExt = ".e.gob"
 
+// TimestampFormat specifies the timestamp format for Time.Format().
+const TimestampFormat = "2006-01-02 15:04:05"
+
 // -----------------------------------------------------------------------------
 
 // Action specifies archiving actions, for example:
--- main.go
+++ main.go
@@ -7,10 +7,9 @@ import (
 	"fmt"
 	"os"
 	"time"
-)
 
-// TimestampFormat specifies the timestamp format for Time.Format().
-const TimestampFormat = "2006-01-02 15:04:05"
+	"github.com/balacode/go-backup/consts"
+)
 
 func main() {
 	runCommand(os.Args)
@@ -20,14 +19,14 @@ func main() {
 // (You could pass it a different set of arguments for testing.)
 func runCommand(osArgs []string) {
 	started := time.Now()
-	fmt.Println(started.Format(TimestampFormat) + ": started")
+	fmt.Println(started.Format(consts.TimestampFormat) + ": started")
 	{
 		// TODO: add code to do actual work here
 	}
 	ended := time.Now()
 	dur := ended.Sub(started)
-	fmt.Println(started.Format(TimestampFormat) + ": started")
-	fmt.Println(ended.Format(TimestampFormat) + ": ended")
+	fmt.Println(started.Format(consts.TimestampFormat) + ": started")
+	fmt.Println(ended.Format(consts.TimestampFormat) + ": ended")
 	fmt.Println("time spent:", dur)
 }
 
