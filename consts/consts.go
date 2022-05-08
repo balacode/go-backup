1aebc5a9932d6810eb02cf192944be8a82d08323
Ali Bala <balarabe@protonmail.com>
2022-05-08 10:15:37 +0100

    refactor: move consts.Version to consts/version.go

--- consts/consts.go
+++ consts/consts.go
@@ -3,9 +3,6 @@
 
 package consts
 
-// Version is the current version of this utility.
-const Version = "0.0.1"
-
 // ArchiveExt specifies the file extension of archives.
 const ArchiveExt = ".e.gob"
 
---
+++ consts/version.go
@@ -0,0 +1,9 @@
+// -----------------------------------------------------------------------------
+// go-backup/consts/version.go
+
+package consts
+
+// Version is the current version of this utility.
+const Version = "0.0.1"
+
+// end
