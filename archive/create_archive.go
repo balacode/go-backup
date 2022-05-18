0791048e7300dcc012fa870f3666db48e1bc3103
Ali Bala <balarabe@protonmail.com>
2022-05-18 22:06:17 +0100

    feat: implement CreateArchive()

--- archive/create_archive.go
+++ archive/create_archive.go
@@ -4,7 +4,13 @@
 package archive
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/balacode/go-backup/logging"
 	"github.com/balacode/go-backup/security"
+	"github.com/balacode/go-backup/storage"
 )
 
 // CreateArchive creates an archive in 'archiveFile', by compressing and
@@ -14,7 +20,63 @@ func CreateArchive(
 	archiveFile string,
 	enc *security.Encryption,
 ) error {
-	panic("NOT IMPLEMENTED: CreateArchive") // TODO
+	if err := enc.Validate(); err != nil {
+		return logging.Error(0xE3F9D1, err)
+	}
+	if storage.ExistsPath(archiveFile) {
+		msg := "file already exists: " + archiveFile
+		return logging.Error(0xE85E89, msg)
+	}
+	archive, err := os.Create(archiveFile)
+	if err != nil {
+		return logging.Error(0xE5C48A, err)
+	}
+	defer archive.Close()
+	sn := -1
+	filepath.Walk(
+		backupPath,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return logging.Error(0xE46E8F, err)
+			}
+			if info.IsDir() { // ignore directories
+				return nil
+			}
+			sn++
+			fl, err := storage.ReadFile(path)
+			if err != nil {
+				return logging.Error(0xE0E1D1, err)
+			}
+			fl.SetRelativePath(backupPath)
+			if fl.Size != uint64(info.Size()) {
+				msg := fmt.Sprintf("fl.Size:%v != info.Size():%v",
+					fl.Size, info.Size())
+				return logging.Error(0xE73B08, msg)
+			}
+			pos := int64(0)
+			{
+				info, err := archive.Stat()
+				if err != nil {
+					return logging.Error(0xE7E2F8, err)
+				}
+				pos = info.Size()
+			}
+			if err := storage.WriteBOFMark(archive); err != nil {
+				return logging.Error(0xE62CF9, err)
+			}
+			if err := fl.WriteEncryptedMetadata(archive, enc); err != nil {
+				return logging.Error(0xE9D64C, err)
+			}
+			if fl.Size > 0 {
+				if err := fl.WriteEncryptedContent(archive, enc); err != nil {
+					return logging.Error(0xE6CE17, err)
+				}
+			}
+			fmt.Printf("archive #%v @%v %vb %v\n", sn, pos, fl.Size, fl.Path)
+			return nil
+		},
+	)
+	return nil
 }
 
 // end
